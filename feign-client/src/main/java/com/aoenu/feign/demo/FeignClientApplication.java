8b3ce7e0c58726373416566ab202c0aa84eaeb66
aoenu <baoben.wu@hand-china.com>
2019-04-25 10:32:53 +0800

    first commit

---
+++ feign-client/src/main/java/com/aoenu/feign/demo/FeignClientApplication.java
@@ -0,0 +1,13 @@
+package com.aoenu.feign.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignClientApplication.class, args);
+    }
+
+}
---
+++ feign-server/src/main/java/com/aoenu/feign/file/FeignFileOperationApplication.java
@@ -0,0 +1,13 @@
+package com.aoenu.feign.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignFileOperationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignFileOperationApplication.class, args);
+    }
+
+}
